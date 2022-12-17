767f762785f9ec1164b434e5004cad6581672baf
GMiculek <garrett.miculek@gmail.com>
2022-12-17 17:37:28 -0600

    Update common.js

--- server/frontend/common.js
+++ server/frontend/common.js
@@ -18,7 +18,12 @@ function query() {
 function login(){
 	
 
-		
+	var user= document.getElementById("Username").value;
+	var pass= document.getElementById("Password").value;
+	document.writeln("<h1>Confirmation Page</h1><br>");
+	document.writeln("Thank you for completing this form.<br><br>");
+	document.writeln("The first name you entered is " + firstname + "<br>");
+	document.writeln("The last name you entered is " + lastname);	
 		
 	
 	
